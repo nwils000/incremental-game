14c4e73a163cd1db8401edf6d2e8209e507df9e5
nwils000 <nwils000@gmail.com>
2024-04-13 15:06:29 -0400

    got a basic auto clicking mechanism working

--- main.js
+++ main.js
@@ -2,8 +2,20 @@ let clickMeButton = document.querySelector('#click-me-button');
 let totalAmountHeading = document.querySelector('#total-amount-heading');
 
 let totalAmount = 0;
+let autoClickInterval = 500;
+let intervalId = null;
+
+function autoClicker() {
+  totalAmount++;
+  totalAmountHeading.textContent = totalAmount;
+}
 
 clickMeButton.addEventListener('click', () => {
   totalAmount++;
   totalAmountHeading.textContent = totalAmount;
+
+  if (totalAmount >= 10) {
+    clearInterval(intervalId);
+    intervalId = setInterval(autoClicker, autoClickInterval);
+  }
 });
